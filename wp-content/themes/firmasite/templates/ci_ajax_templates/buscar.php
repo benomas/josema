875edd9078b320b950cf2db17dadb9241f8b0d69
Benomas <benomas@gmail.com>
2018-08-22 23:53:36 -0500

    Se agrega mecanismo para filtrar desde menu

--- wp-content/themes/firmasite/templates/ci_ajax_templates/buscar.php
+++ wp-content/themes/firmasite/templates/ci_ajax_templates/buscar.php
@@ -5,8 +5,17 @@
 // this one letting us translate page template names
 ?>
 <script>
+	var url = '<?php echo get_rcodeigniter_access_url('inventario/buscar/');?>'
+	<?php
+		if(!empty($_GET["basic_filter_name"]) && !empty($_GET["basic_filter_value"]))
+		{
+	?>
+		url += '0?basic_filter_name=<?php echo $_GET["basic_filter_name"];?>&basic_filter_value=<?php echo $_GET["basic_filter_value"];?>'
+	<?php
+		}
+	?>
 	$.ajax({
-			url : '<?php echo get_rcodeigniter_access_url('inventario/buscar/');?>',
+			url,
 			type: 'POST',
 			success : function(html)
 			{
